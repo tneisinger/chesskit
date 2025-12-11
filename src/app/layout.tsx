bd557ccea059180a22cd952e029682c913a77a50
Tyler Neisinger <tjneisi@gmail.com>
2025-12-11 12:48:43 -0800

    Improve appearance of LessonDisplay and openings page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navigation />
-        {children}
+        <div className="flex justify-center max-w-[1440px] mx-auto">
+          {children}
+        </div>
       </body>
     </html>
   );
