ffad35e11a5697f568ca6a6b570a5f1f1df9754c
Tyler Neisinger <tjneisi@gmail.com>
2026-01-03 10:46:05 -0800

    Improve appearance of EvalerDisplay

--- src/components/evalerLine.tsx
+++ src/components/evalerLine.tsx
@@ -1,28 +1,35 @@
-import React from 'react'
 import { makeScoreString, MultiPV } from '@/utils/stockfish';
 import { convertLanLineToSanLine } from '@/utils/chess';
 
 interface Props {
   fen: string;
+  maxLineLengthPx: number;
   line?: MultiPV;
-  maxLineLength?: number;
 }
 
-const EvalerLine = ({ fen, line, maxLineLength = 5 }: Props) => {
+const EvalerLine = ({ fen, maxLineLengthPx, line, }: Props) => {
   if (line == undefined) return <div className="py-[3px] h-[1.6rem]" />;
 
   const sanLine = convertLanLineToSanLine(line.lanLine, fen);
 
+  const maxMovesLengthPx = maxLineLengthPx - 54; // 54px accounts for score and arrow
+  const approxCharWidthPx = 10; // approximate width of a character in pixels
+  const maxMovesChars = Math.floor(maxMovesLengthPx / approxCharWidthPx);
+  let truncatedSanLine = "";
+  for (let i = 0, len = sanLine.length; i < len; i++) {
+    const move = sanLine[i];
+    if ((truncatedSanLine + move).length > maxMovesChars) {
+      break;
+    }
+    truncatedSanLine += move + " ";
+  }
+
   return (
-    <div className="py-[3px] h-[1.6rem] text-sm">
-      <span className="inline-block w-10 text-right">{makeScoreString(line.score)}</span>
+    <div className="text-md ml-2">
+      <span className="inline-block text-right">{makeScoreString(line.score)}</span>
       <span className="inline-block w-[24px] text-center">{'->'}</span>
       <span className="inline-block text-center">
-        {sanLine.slice(0, maxLineLength).map((move, i) =>
-          <span key={`${line.multipv} ${i}`} className="inline-block text-center [&+span]:ml-2">
-            {move}
-          </span>
-        )}
+        {truncatedSanLine}
       </span>
     </div>
   )
