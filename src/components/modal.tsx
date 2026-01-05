508ead75b12e9a8fcfa300aca92d3a4038a7e1ad
Tyler Neisinger <tjneisi@gmail.com>
2026-01-04 16:09:29 -0800

    Bugfix: make modal background cover entire screen

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -14,7 +14,7 @@ const Modal = (props: Props) => {
   if (props.show) {
     return (
       <>
-        <div className="fixed left-0 top-0 size-dvw bg-black opacity-25 z-1000" />
+        <div className="fixed left-0 top-0 w-dvw h-dvh bg-black opacity-25 z-1000" />
         <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-2000">{props.children}</div>
       </>
     );
