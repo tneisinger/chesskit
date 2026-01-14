d8588d1a39735f66b5198bce7fbd1c5539ef18cf
Tyler Neisinger <tjneisi@gmail.com>
2026-01-13 19:52:22 -0800

    Bring over fetch code from chessflowOld

---
+++ src/fetch/chesscom.ts
@@ -0,0 +1,124 @@
+import { parse as parsePGN } from 'pgn-parser';
+import hash from 'string-hash';
+import { GameData, PieceColor } from '@/types/chess';
+import type { ChesscomGame } from '@/types/chesscom';
+import {
+  getStartTime,
+  getGameResult,
+  getTimeControl,
+  getPlayerElo,
+  getPlayerName,
+} from '@/utils/pgn'
+import { DEFAULT_MAX_GAMES, Options, Games } from './common';
+import { FEN } from 'cm-chess/src/Chess';
+
+export function getUserColor(game: ChesscomGame, username: string): PieceColor | null {
+  if (game.white.username === username) return PieceColor.WHITE;
+  if (game.black.username === username) return PieceColor.BLACK;
+  return null;
+}
+
+/**
+ * Get a user's recent chess.com games
+ */
+export async function fetchChesscomGames(
+  username: string,
+  savedGames: Games,
+  options?: Options,
+): Promise<GameData[]> {
+  let games: ChesscomGame[] = [];
+
+  let archiveURLs: string[] | undefined = undefined;
+  try {
+    archiveURLs = await fetchChesscomMonthlyArchiveURLs(username);
+  } catch (e) {
+    return Promise.reject(e);
+  }
+
+  if (archiveURLs == undefined) throw new Error('archiveURLs should not be undefined');
+
+  // Get the array of gameIds for all the games that we already have saved
+  const gameIds: string[] = Object.keys(savedGames);
+
+  while (archiveURLs.length > 0 && shouldFetchMoreGames(games.length, options)) {
+    const url = archiveURLs.pop();
+    if (!url) break;
+
+    const response = await fetch(url);
+    if (!response.ok) return Promise.reject(response);
+
+    const json = await response.json();
+
+    // Filter out any games that we already have stored in zustand.
+    const newGames = (json.games as ChesscomGame[]).filter((g) =>
+      !gameIds.includes(makeGameId(g)));
+
+    games = [...games, ...newGames];
+  }
+
+  // Filter out any games that are not standard chess games.
+  games = games.reduce((acc: ChesscomGame[], game) => {
+    if (game.initial_setup !== FEN.start) return acc
+    if (game.rules !== 'chess') return acc;
+    return [...acc, game];
+  }, []);
+
+  // Sort the games by start_time or end_time, from newest to oldest
+  games.sort((g1, g2) => {
+    const t1 = g1.start_time ? g1.start_time : g1.end_time;
+    const t2 = g2.start_time ? g2.start_time : g2.end_time;
+    if (t1 && t2) return t2 - t1;
+    return 0;
+  });
+
+  if (options && options.maxGames) games = games.slice(0, options.maxGames);
+
+  // Convert ChesscomGame[] to GameData[] and return it.
+  return games.map((game) => {
+    const pgnString = game.pgn;
+    const pgn = parsePGN(game.pgn)[0];
+    const userColor = getUserColor(game, username);
+    if (userColor == null) {
+      throw new Error(`Failed to get userColor for ${username}`);
+    }
+
+    const startTime = getStartTime(pgn);
+    if (startTime == undefined) {
+      throw new Error('Failed to get startTime from pgn')
+    }
+
+    return {
+      gameId: makeGameId(game),
+      pgn,
+      startTime,
+      result: getGameResult(pgnString),
+      userColor,
+      url: game.url,
+      timeControl: getTimeControl(pgn),
+      whiteName: getPlayerName(pgn, PieceColor.WHITE),
+      whiteElo: getPlayerElo(pgn, PieceColor.WHITE),
+      blackName: getPlayerName(pgn, PieceColor.BLACK),
+      blackElo: getPlayerElo(pgn, PieceColor.BLACK),
+    };
+  });
+}
+
+// Predicate to determine if more games should be fetched from chess.com
+function shouldFetchMoreGames(numGames: number, options?: Options): boolean {
+  if (options && options.maxGames) return numGames < options.maxGames;
+  return numGames < DEFAULT_MAX_GAMES;
+}
+
+export async function fetchChesscomMonthlyArchiveURLs(
+  username: string,
+): Promise<string[]> {
+  const url = `https://api.chess.com/pub/player/${username}/games/archives`;
+  const response = await fetch(url);
+  if (!response.ok) return Promise.reject(response);
+  const json = await response.json();
+  return json.archives;
+}
+
+function makeGameId(game: ChesscomGame): string {
+  return hash(game.url).toString();
+}
