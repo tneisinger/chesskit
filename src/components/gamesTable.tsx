c3b611e54d50e5b0d6a3d18d6101743b43b3f11f
Tyler Neisinger <tjneisi@gmail.com>
2026-01-17 13:39:36 -0800

    Improve appearance of game-review page

--- src/components/gamesTable.tsx
+++ src/components/gamesTable.tsx
@@ -1,32 +1,19 @@
 import type { GameData } from '@/types/chess';
 import GamesTableRow from '@/components/gamesTableRow';
-import { shouldUseMobileLayout } from '../utils/mobileLayout';
-import DeleteGamesControlPanel from '../components/deleteGamesControlPanel';
-import useWindowSize from '@/hooks/useWindowSize';
 
 interface Props {
   games: GameData[];
   selectedGameIds: string[];
   changeSelectedGameIds: (newGameIds: string[]) => void;
-  includeDeleteControlPanel?: boolean;
-  maxHeight?: string;
-  maxHeightMobile?: string;
 }
 
 const GamesTable = ({
   games,
   selectedGameIds,
   changeSelectedGameIds,
-  includeDeleteControlPanel = false,
-  maxHeight = '95%',
-  maxHeightMobile = '95%',
 }: Props) => {
-  const windowSize = useWindowSize();
-
-  if (shouldUseMobileLayout(windowSize)) maxHeight = maxHeightMobile;
-
   return (
-    <div className="flex flex-col w-full max-w-[95vw]" style={{ maxHeight }}>
+    <div className="flex flex-col w-full max-w-[95vw] h-full">
       <GamesTableRow
         changeSelectedGameIds={changeSelectedGameIds}
         selectedGameIds={selectedGameIds}
@@ -39,15 +26,6 @@ const GamesTable = ({
             isOdd={i % 2 === 0}
             changeSelectedGameIds={changeSelectedGameIds}
             selectedGameIds={selectedGameIds}
-          />
-        )}
-      </div>
-      <div className="flex flex-row">
-        {includeDeleteControlPanel && (
-          <DeleteGamesControlPanel
-            games={games}
-            selectedGameIds={selectedGameIds}
-            changeSelectedGameIds={changeSelectedGameIds}
           />
         )}
       </div>
