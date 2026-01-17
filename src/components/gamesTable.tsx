2e8dc221251f24e8e31edbf4ec61ee7ebfa30e3d
Tyler Neisinger <tjneisi@gmail.com>
2026-01-17 11:59:17 -0800

    Remove usage of react-paginate

--- src/components/gamesTable.tsx
+++ src/components/gamesTable.tsx
@@ -1,9 +1,6 @@
-import { useState } from 'react';
 import type { GameData } from '@/types/chess';
 import GamesTableRow from '@/components/gamesTableRow';
-import styles from './gamesTable.module.css';
 import { shouldUseMobileLayout } from '../utils/mobileLayout';
-import ReactPaginate, { ReactPaginateProps } from 'react-paginate';
 import DeleteGamesControlPanel from '../components/deleteGamesControlPanel';
 import useWindowSize from '@/hooks/useWindowSize';
 
@@ -16,8 +13,6 @@ interface Props {
   maxHeightMobile?: string;
 }
 
-const gamesPerPage = 25
-
 const GamesTable = ({
   games,
   selectedGameIds,
@@ -28,18 +23,6 @@ const GamesTable = ({
 }: Props) => {
   const windowSize = useWindowSize();
 
-  const [offset, setOffset] = useState(0);
-
-  const endOffset = offset + gamesPerPage;
-  const gamesOnCurrentPage = games.slice(offset, endOffset);
-  const pageCount = Math.ceil(games.length / gamesPerPage);
-
-  const handlePageClick: ReactPaginateProps['onPageChange'] = (event) => {
-    const newOffset = (event.selected * gamesPerPage) % games.length;
-    console.log('changing to page', event.selected)
-    setOffset(newOffset);
-  }
-
   if (shouldUseMobileLayout(windowSize)) maxHeight = maxHeightMobile;
 
   return (
@@ -49,7 +32,7 @@ const GamesTable = ({
         selectedGameIds={selectedGameIds}
       />
       <div className="min-h-0 flex-1 rounded-md overflow-x-hidden overflow-y-scroll">
-        {gamesOnCurrentPage.map((game, i) =>
+        {games.map((game, i) =>
           <GamesTableRow
             key={game.gameId}
             game={game}
@@ -62,23 +45,10 @@ const GamesTable = ({
       <div className="flex flex-row">
         {includeDeleteControlPanel && (
           <DeleteGamesControlPanel
-            games={gamesOnCurrentPage}
+            games={games}
             selectedGameIds={selectedGameIds}
             changeSelectedGameIds={changeSelectedGameIds}
           />
-        )}
-        {games.length > gamesOnCurrentPage.length && (
-          <div className={styles.paginator}>
-            <ReactPaginate
-              breakLabel='...'
-              nextLabel='next >'
-              onPageChange={handlePageClick}
-              pageRangeDisplayed={5}
-              pageCount={pageCount}
-              previousLabel='< previous'
-              renderOnZeroPageCount={null}
-            />
-          </div>
         )}
       </div>
     </div>
