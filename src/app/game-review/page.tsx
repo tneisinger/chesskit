c294bfe137cc44c3931193a487c083baaded6ac7
Tyler Neisinger <tjneisi@gmail.com>
2026-01-21 12:03:24 -0800

    Fix height of main div on game-review page

--- src/app/game-review/page.tsx
+++ src/app/game-review/page.tsx
@@ -2,17 +2,19 @@
 
 import { useState, useEffect } from 'react';
 import Button from '@/components/button';
-import { ScrollLock } from '@/components/ScrollLock';
 import GamesTable from '@/components/gamesTable';
 import DeleteGamesControlPanel from '@/components/deleteGamesControlPanel';
 import type { GameData } from "@/types/chess";
 import { getUserGames } from './actions';
 import { NAV_BAR_HEIGHT } from '@/lib/constants';
+import useWindowSize from '@/hooks/useWindowSize';
 
 export default function GameReviewPage() {
   const [selectedGameIds, setSelectedGameIds] = useState<number[]>([]);
   const [userGames, setUserGames] = useState<GameData[]>([]);
   const [isLoading, setIsLoading] = useState(true);
+
+  const { height } = useWindowSize();
 
   const loadGames = async () => {
     setIsLoading(true);
@@ -30,28 +32,29 @@ export default function GameReviewPage() {
   }
 
   return (
-    <ScrollLock>
-      <div className={`flex flex-col max-w-[1200px] w-[99vw] h-[calc(100vh-${NAV_BAR_HEIGHT}px)]`}>
-        <div className="py-4">
-          <h3 className="text-center text-2xl">Imported Games</h3>
-        </div>
-        <div className="flex-1 min-h-0">
-          <GamesTable
-            games={userGames}
-            selectedGameIds={selectedGameIds}
-            changeSelectedGameIds={setSelectedGameIds}
-          />
-        </div>
-        <div className="py-2 flex flex-row gap-4 items-center justify-between">
-          <DeleteGamesControlPanel
-            selectedGameIds={selectedGameIds}
-            changeSelectedGameIds={setSelectedGameIds}
-            games={userGames}
-            onGamesDeleted={loadGames}
-          />
-          <Button href="/game-review/add-games">Add Games</Button>
-        </div>
+    <div
+      className="flex flex-col max-w-[1200px] w-[99vw]"
+      style={{ height: height ? height - NAV_BAR_HEIGHT : undefined }}
+    >
+      <div className="py-4">
+        <h3 className="text-center text-2xl">Imported Games</h3>
       </div>
-    </ScrollLock>
+      <div className="flex-1 min-h-0">
+        <GamesTable
+          games={userGames}
+          selectedGameIds={selectedGameIds}
+          changeSelectedGameIds={setSelectedGameIds}
+        />
+      </div>
+      <div className="py-2 flex flex-row gap-4 items-center justify-between">
+        <DeleteGamesControlPanel
+          selectedGameIds={selectedGameIds}
+          changeSelectedGameIds={setSelectedGameIds}
+          games={userGames}
+          onGamesDeleted={loadGames}
+        />
+        <Button href="/game-review/add-games">Add Games</Button>
+      </div>
+    </div>
   );
 }
