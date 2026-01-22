c75356b996c2b03829fa851553a7668751bb93e7
Tyler Neisinger <tjneisi@gmail.com>
2026-01-21 16:55:54 -0800

    Create custom tooltip for GameChart

--- src/components/gameChart.tsx
+++ src/components/gameChart.tsx
@@ -1,4 +1,4 @@
-import { GameEvals } from '@/types/chess';
+import { GameData, GameEvals } from '@/types/chess';
 import {
   AreaChart,
   Area,
@@ -9,12 +9,18 @@ import {
   ReferenceLine,
 } from 'recharts';
 import { Move } from 'cm-chess/src/Chess';
-import { getPlyFromFen } from '@/utils/chess';
+import { getFenParts, getPlyFromFen } from '@/utils/chess';
+import { FEN } from 'cm-chessboard/src/Chessboard';
+import { Chess as ChessJS } from 'chess.js';
+import GameChartToolTip from '@/components/gameChartToolTip';
 
 const CHART_MAX_CP = 1000;
 
 interface ChartDataPointBaseType {
   ply: number;
+  move: string;
+  color: 'w' | 'b';
+  moveNumber: number;
   chartCp: number;
 }
 
@@ -32,24 +38,46 @@ interface ChartDataPointWithMate extends ChartDataPointBaseType {
 // A data point must have either cp or mate, but not both
 type ChartDataPoint = ChartDataPointWithCP | ChartDataPointWithMate;
 
-function makeChartData(gameEvals: GameEvals): ChartDataPoint[] {
+function makeChartData(game: GameData, gameEvals: GameEvals): ChartDataPoint[] {
   const result: ChartDataPoint[] = [];
 
+  const chessjs = new ChessJS();
+  chessjs.loadPgn(game.pgn);
+  const history = chessjs.history({ verbose: true });
+
   Object.entries(gameEvals).forEach(([fen, e]) => {
+    // Skip starting position
+    if (fen === FEN.start) return;
+
+    const moveInfo = history.find((m) => m.after === fen);
+    if (!moveInfo) {
+      console.warn('Failed to find move info in history for fen:', fen)
+      console.warn('history:');
+      console.warn(history);
+      throw new Error(`Could not find move info for fen ${fen}`);
+    }
+
+    const move = moveInfo.san;
+    const { fullMoveNumber: moveNumber } = getFenParts(moveInfo.before);
+    const color = moveInfo.color;
+
+
     const ply = getPlyFromFen(fen);
+    const partial = { moveNumber, move, color, ply };
+
     if (e.cp !== undefined) {
       let chartCp = e.cp;
       if (Math.abs(chartCp) > CHART_MAX_CP) {
         chartCp = chartCp > 0 ? CHART_MAX_CP : -CHART_MAX_CP
       }
-      result.push({ ply, chartCp, cp: e.cp });
+      result.push({ ...partial, chartCp, cp: e.cp });
     } else if (e.mate !== undefined) {
       // Represent mate as CHART_MAX_CP with sign indicating who is mating
       const sign = e.mate > 0 ? 1 : -1;
-      result.push({ ply, chartCp: sign * CHART_MAX_CP, mate: e.mate });
+      result.push({ ...partial, chartCp: sign * CHART_MAX_CP, mate: e.mate });
     } else {
       // No evaluation available so set cp to 0
-      result.push({ ply, chartCp: 0, cp: 0 });
+      result.push({ ...partial, chartCp: 0, cp: 0 });
     }
   });
 
@@ -58,18 +86,20 @@ function makeChartData(gameEvals: GameEvals): ChartDataPoint[] {
 }
 
 interface Props {
+  game: GameData;
   gameEvals: GameEvals;
   currentMove: Move | undefined;
   width: number;
 }
 
 const GameChart = ({
+  game,
   gameEvals,
   currentMove,
   width,
 }: Props) => {
 
-  const chartData = makeChartData(gameEvals);
+  const chartData = makeChartData(game, gameEvals);
   const max = Math.max(...chartData.map((d) => d.chartCp));
   const min = Math.min(...chartData.map((d) => d.chartCp));
   const offset = max / (max - min);
@@ -86,7 +116,7 @@ const GameChart = ({
         <XAxis dataKey="ply" hide />
         <YAxis domain={[-CHART_MAX_CP, CHART_MAX_CP]} hide />
         <ReferenceLine x={currentMove ? currentMove.ply : 0} stroke="white" />
-        <Tooltip />
+        <Tooltip content={GameChartToolTip} />
         <Area
           type="monotone"
           dataKey="chartCp"
