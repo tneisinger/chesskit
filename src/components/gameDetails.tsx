90089e7243eefed83c490fd99f8f2a35d4404aea
Tyler Neisinger <tjneisi@gmail.com>
2026-01-23 15:14:44 -0800

    Improve layout and add game result to GameDetails

--- src/components/gameDetails.tsx
+++ src/components/gameDetails.tsx
@@ -8,24 +8,36 @@ interface Props {
 }
 
 const GameDetails = ({ game, orientation }: Props) => {
+  let resultText = 'Result: unknown';
+  if (game.result) {
+    if (game.result === '1-0') resultText = 'Result: White Wins';
+    if (game.result === '0-1') resultText = 'Result: Black Wins';
+    if (game.result === '1/2-1/2') resultText = 'Result: Draw';
+  }
+
   return (
-    <div className="w-full bg-background-page text-sm p-2 rounded-md">
-      <p>{makeDateStringYYMMDD(new Date(game.startTime))}</p>
-      {game.timeControl && (
-        <p>Time Control: {makeReadableTimeControl(game.timeControl)}</p>
-      )}
+    <div className="flex flex-col w-full bg-background-page text-sm rounded-md gap-3">
+      <section className="flex flex-row w-full justify-between bg-stone-700 p-2">
+        <p>{makeDateStringYYMMDD(new Date(game.startTime))}</p>
+        {game.timeControl && (
+          <p>{makeReadableTimeControl(game.timeControl)}</p>
+        )}
+      </section>
 
       {/* Players Section - Use orientation to order the players to match the board orientation */}
-      <section className={`my-2 flex ${orientation === PieceColor.WHITE ? 'flex-col' : 'flex-col-reverse'} my-2`}>
+      <section className={`flex ${orientation === PieceColor.WHITE ? 'flex-col' : 'flex-col-reverse'} ml-6`}>
         <p>{game.blackName} - {game.blackElo}</p>
         <p>{game.whiteName} - {game.whiteElo}</p>
       </section>
 
-      {game.website && game.url && (
-        <a target="_blank" rel="noopener noreferrer" href={game.url} className="underline">
-          View at {game.website}
-        </a>
-      )}
+      <section className="text-center border-t border-stone-600 p-2">
+        <p>{resultText}</p>
+        {game.website && game.url && (
+          <a target="_blank" rel="noopener noreferrer" href={game.url} className="underline">
+            View on {game.website}
+          </a>
+        )}
+      </section>
     </div>
   )
 };
