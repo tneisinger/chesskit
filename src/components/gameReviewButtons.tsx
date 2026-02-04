91ca9158baf9a6da893e485b80e445abe5646954
Tyler Neisinger <tjneisi@gmail.com>
2026-02-03 16:51:25 -0800

    Change when MakeFlashcardBtn highlight and enable

--- src/components/gameReviewButtons.tsx
+++ src/components/gameReviewButtons.tsx
@@ -6,11 +6,6 @@ import { makeMoveJudgements } from "@/utils/chess";
 import { getColor, isInVariation } from "@/utils/cmchess";
 import CreateFlashcardModal from "./createFlashcardModal";
 
-const highlightedJudgements: MoveJudgement[] = [
-  MoveJudgement.Blunder,
-  MoveJudgement.Mistake,
-]
-
 interface Props {
   game: GameData;
   gameEvaluation: GameEvaluation;
@@ -32,26 +27,53 @@ const GameReviewButtons = ({
   const getBestLines = useCallback(() => {
     if (!currentMove) return undefined;
     const posEval = gameEvaluation[currentMove.fen];
-    return posEval?.lines;
+    return posEval.lines;
   }, [currentMove, gameEvaluation]);
 
-  const shouldHighlightFlashcardBtn = useCallback(() => {
+
+  const shouldHighlightFlashcardBtn = useCallback((): boolean => {
+    // Don't highlight the button if in the starting position.
     if (currentMove === undefined) return false;
+
+    // Don't highlight the button if we are in a variation.
     if (isInVariation(currentMove)) return false;
+
+    // If it is the user's turn and there is a next move...
     if (game.userColor !== getColor(currentMove) && currentMove.next) {
+      // Highlight the button if the judgement of the next move is bad enough
+      const highlightedJudgements: MoveJudgement[] = [
+        MoveJudgement.Blunder,
+        MoveJudgement.Mistake,
+      ]
       const j = moveJudgements[currentMove.next.fen]
       return highlightedJudgements.includes(j);
     }
+    return false;
   }, [currentMove, game])
 
-  const bestLines = getBestLines();
-
-  const shouldDisableFlashcardBtn = useCallback(() => {
-    if (bestLines == undefined) return true;
+  const shouldDisableFlashcardBtn = useCallback((): boolean => {
     if (currentMove == undefined) return true;
     if (isInVariation(currentMove)) return true;
-    return game.userColor === getColor(currentMove);
+
+    // If it is the users turn and there is a next move...
+    if (game.userColor !== getColor(currentMove) && currentMove.next) {
+      // If the judgement of the next move is bad enough, enable the
+      // 'Make Flashcard' button.
+      const btnEnabledJudgements = [
+        MoveJudgement.Blunder,
+        MoveJudgement.Mistake,
+        MoveJudgement.Inaccurate,
+      ];
+      const j = moveJudgements[currentMove.next.fen]
+      return !btnEnabledJudgements.includes(j);
+    }
+
+    // Otherwise, the button should be disabled.
+    return true;
   }, [currentMove, game]);
+
+
+  const bestLines = getBestLines();
 
   return (
     <div className="p-2 flex flex-col justify-center items-center w-full bg-background-page rounded-md gap-3">
