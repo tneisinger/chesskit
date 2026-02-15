ec86bf2e4d0ea4cb07ab54c5d6f21ddf650b15df
Tyler Neisinger <tjneisi@gmail.com>
2026-02-15 10:31:51 -0800

    Add external url of game to flashcard details

--- src/components/flashcardDetails.tsx
+++ src/components/flashcardDetails.tsx
@@ -8,14 +8,20 @@ interface Props {
 const FlashcardDetails = ({ flashcard }: Props) => {
   return (
     <div className="flex flex-col bg-background-page rounded-md">
-      <h3 className="text-lg text-center font-bold bg-stone-700 p-1 rounded-t-md mb-1">Card Details</h3>
-      <div className="flex flex-col items-center gap-1 mb-2 text-md">
-        <p>Flashcard: #{flashcard.id}</p>
+      <h3 className="text-lg text-center font-bold bg-stone-700 p-1 rounded-t-md mb-2">Card Details</h3>
+      <div className="flex flex-col items-center gap-2 mb-2 text-sm">
         <p>Created: {flashcard.createdAt.toLocaleDateString()}</p>
-        <p>Last Attempted: {flashcard.lastReviewedDate ? flashcard.lastReviewedDate.toLocaleDateString() : 'Never'}</p>
-        {flashcard.gameId !== null && (
-          <Link href={`/game-review/${flashcard.gameId}`} className="text-white underline">Game Review</Link>
-        )}
+        <p>Last Attempt: {flashcard.lastReviewedDate ? flashcard.lastReviewedDate.toLocaleDateString() : 'Never'}</p>
+        <div className="pt-2 px-4 flex flex-row justify-between w-full border-t-1 border-stone-700">
+            {flashcard.gameId != null && (
+              <Link href={`/game-review/${flashcard.gameId}`} className="text-white underline">Game Review</Link>
+            )}
+            {flashcard.gameUrl != null && (
+              <a target="_blank" rel="noopener noreferrer" href={flashcard.gameUrl} className="underline">
+                View on {(new URL(flashcard.gameUrl)).hostname}
+              </a>
+            )}
+          </div>
       </div>
     </div>
   );
