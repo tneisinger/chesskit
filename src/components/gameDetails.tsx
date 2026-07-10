3768baf85bbe52f53ad25fd74fb58386d6b1ff89
Tyler Neisinger <tjneisi@gmail.com>
2026-07-10 10:25:14 -0700

    Add opening to GameDetails component

--- src/components/gameDetails.tsx
+++ src/components/gameDetails.tsx
@@ -1,6 +1,10 @@
 import { GameData, PieceColor } from '@/types/chess';
 import { makeReadableTimeControl } from '@/utils/chess';
 import { makeDateStringYYMMDD } from '@/utils';
+import { getOpening } from '@/utils/bookPositions';
+
+const MAX_OPPONENT_NAME_LENGTH = 15;
+
 
 interface Props {
   game: GameData;
@@ -15,6 +19,20 @@ const GameDetails = ({ game, orientation }: Props) => {
     if (game.result === '1/2-1/2') resultText = 'Result: Draw';
   }
 
+  let whiteName;
+  if (game.whiteName != undefined) {
+    whiteName = game.whiteName.length > MAX_OPPONENT_NAME_LENGTH ?
+      game.whiteName.slice(0, MAX_OPPONENT_NAME_LENGTH) + '...' :
+      game.whiteName;
+  }
+
+  let blackName;
+  if (game.blackName != undefined) {
+    blackName = game.blackName.length > MAX_OPPONENT_NAME_LENGTH ?
+      game.blackName.slice(0, MAX_OPPONENT_NAME_LENGTH) + '...' :
+      game.blackName;
+  }
+
   return (
     <div className="flex flex-col w-full bg-background-page text-sm rounded-md gap-3">
       <section className="flex flex-row w-full justify-between bg-stone-700 p-2">
@@ -26,8 +44,12 @@ const GameDetails = ({ game, orientation }: Props) => {
 
       {/* Players Section - Use orientation to order the players to match the board orientation */}
       <section className={`flex ${orientation === PieceColor.WHITE ? 'flex-col' : 'flex-col-reverse'} ml-6`}>
-        <p>{game.blackName} - {game.blackElo}</p>
-        <p>{game.whiteName} - {game.whiteElo}</p>
+        <p>{blackName == undefined ? "(Name Unknown)" : blackName} - {game.blackElo == undefined ? "???" : game.blackElo}</p>
+        <p>{whiteName == undefined ? "(Name Unknown)" : whiteName} - {game.whiteElo == undefined ? "???" : game.whiteElo}</p>
+      </section>
+
+      <section className="flex flex-row w-full ml-6">
+        <p>{getOpening(game)}</p>
       </section>
 
       <section className="text-center border-t border-stone-600 p-2">
